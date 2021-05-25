a3da7079658c216699b667896e58e409e1452835
Gergely Brautigam <skarlso777@gmail.com>
2021-05-25 17:56:11 +0200

    Clean slate

--- README.md
+++
@@ -1,2 +0,0 @@
-# skarlso.github.io
-This is my Blog
--- js/main.min.4acd331997be4b64e30f47c568e49ba9ad887432eb559fcd232d73a3860134c5.js
+++
@@ -1,7 +0,0 @@
-const listen=(ele,e,callback)=>{if(document.querySelector(ele)!==null){document.querySelector(ele).addEventListener(e,callback);}}
-let header=document.getElementById('site-header');let lastScrollPosition=window.pageYOffset;let mobileMenuVisible=false;const toggleMobileMenu=()=>{let mobileMenu=document.getElementById('mobile-menu');if(mobileMenuVisible==false){mobileMenu.style.animationName='bounceInRight';mobileMenu.style.webkitAnimationName='bounceInRight';mobileMenu.style.display='block';mobileMenuVisible=true;}else{mobileMenu.style.animationName='bounceOutRight';mobileMenu.style.webkitAnimationName='bounceOutRight'
-mobileMenuVisible=false;}}
-const showImg=()=>{document.querySelector('.bg-img').classList.add('show-bg-img');}
-const hideImg=()=>{document.querySelector('.bg-img').classList.remove('show-bg-img');}
-const toggleToc=()=>{document.getElementById('toc').classList.toggle('show-toc');}
-if(header!==null){listen('#menu-btn',"click",toggleMobileMenu);listen('#toc-btn',"click",toggleToc);listen('#img-btn',"click",showImg);listen('.bg-img',"click",hideImg);}
